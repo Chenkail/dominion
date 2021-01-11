f90b51aab054e7d22313d89169ae5abc81644a0c
Chenkail <40770208+Chenkail@users.noreply.github.com>
2021-01-11 06:03:14 -0800

    Formatting

--- dominion/src/main.rs
+++ dominion/src/main.rs
@@ -2,9 +2,9 @@ mod game;
 use game::prelude::*;
 
 fn main() {
-    let kingdom_cards: Vec<Box<dyn Card>> = vec![Box::new(Cellar), Box::new(Market), Box::new(Merchant), Box::new(Militia), 
-                                                    Box::new(Mine), Box::new(Moat), Box::new(Remodel), Box::new(Smithy), 
-                                                    Box::new(Village), Box::new(Workshop)];
+    let kingdom_cards: Vec<Box<dyn Card>> = 
+        vec![Box::new(Cellar), Box::new(Market), Box::new(Merchant), Box::new(Militia), Box::new(Mine), 
+            Box::new(Moat), Box::new(Remodel), Box::new(Smithy), Box::new(Village), Box::new(Workshop)];
     let mut game = Game::new(4, kingdom_cards);
     let mut player = Player::new();
     player.print_state();
