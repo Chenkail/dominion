4814e9fa93652b96f89ec3203e7f91ac31fd491d
Chenkail <40770208+Chenkail@users.noreply.github.com>
2021-01-22 15:05:30 -0800

    Callback docs

--- src/game/callbacks.rs
+++ src/game/callbacks.rs
@@ -1,9 +1,13 @@
+//! The callbacks that need to be provided when building a client
+
 use crate::game::player::Player;
 
 pub type FnToBool = Box<dyn Fn() -> bool>;
 pub type FnPlayerToi32 = Box<dyn Fn(&Player) -> i32>;
 
 pub struct Callbacks {
+    /// Is the player done with this phase
     pub prompt_player_done: FnToBool,
+    /// Get an index of a card in hand to choose to play
     pub prompt_card_from_hand: FnPlayerToi32,
 }
