8e2e4ea3d572f39af0b4f5c2895eebf19980aeab
maxcaseylin <maxcaseylin@gmail.com>
2021-01-22 16:11:13 -0600

    add test_player_init

--- tests/test_player.rs
+++ tests/test_player.rs
@@ -1,10 +1,13 @@
 #[cfg(test)]
 mod test_player {
-    use dominion::game::player;
-    use dominion::game;
+    use dominion::game::player::*;
+    use dominion::game::*;
 
     #[test]
-    fn test_test() {
-        assert!(true);
+    fn test_player_init() {
+        let player = Player::default();
+
+        //we check to see if everything is initalized correctly
+        assert!(player.hand.len() == 5);
     }
 }
