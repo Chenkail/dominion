ab6f4f3db6ab47a7c215fc9a95154d4aefdf420c
maxcaseylin <maxcaseylin@gmail.com>
2021-01-22 21:12:22 -0600

    test_vic

--- tests/test_game.rs
+++ tests/test_game.rs
@@ -2,11 +2,23 @@
 
 #[cfg(test)]
 pub mod test_game {
-    use dominion::game::gamedata::*;
+    use dominion::game::{gamedata::*, prelude::*};
     #[test]
     pub fn test_printsupply() {
         let mut game = Game::default();
         game.print_supply();
     }
-    
+
+    #[test]
+    pub fn test_vic_cond() {
+        let mut game = Game::default();
+        game.supply.insert(Box::new(Province), 0);
+        assert!(game.victory_met());
+        game.supply.insert(Box::new(Province), 8);
+        game.supply.insert(Box::new(Copper), 0);
+        game.supply.insert(Box::new(Smithy), 0);
+        game.supply.insert(Box::new(Moat), 0);
+        assert!(game.victory_met());
+    }
+
 }
