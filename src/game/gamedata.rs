ae4cfc9249b4f6c34af06d4bdffe7e1749384c4c
maxcaseylin <maxcaseylin@gmail.com>
2021-01-25 19:25:32 -0600

    update avail to include filter for piles > 0

--- src/game/gamedata.rs
+++ src/game/gamedata.rs
@@ -91,6 +91,9 @@ impl Game {
     /// returns vector of cards available given a lambda passed in
     pub fn return_available_cards(&self) -> Vec<Box<dyn Card>> {
         //TODO: rewrite to not use collect and to use filter() with the lambda passed in
-        return self.supply.keys().cloned().collect();
+        return self.supply.keys()
+            .filter(|a| *self.supply.get(*a).unwrap() > 0)
+            .cloned()
+            .collect();
     }
 }
