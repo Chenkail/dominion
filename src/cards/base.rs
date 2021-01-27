f6c0877dba247c7ba5cb66131cd64daf3b447064
Chenkail <40770208+Chenkail@users.noreply.github.com>
2021-01-26 21:32:19 -0800

    Add wiki links for basic cards

--- src/cards/base.rs
+++ src/cards/base.rs
@@ -11,11 +11,77 @@ impl Card for Copper {
     name!("Copper");
     cost!(0);
     types!(vec![Treasure]);
+    treasure_value!(1);
 }
 
-basic_treasure!(Silver, "Silver", 3, 2);
-basic_treasure!(Gold, "Gold", 6, 3);
-basic_victory!(Estate, "Estate", 2, 1);
-basic_victory!(Duchy, "Duchy", 5, 3);
-basic_victory!(Province, "Province", 8, 6);
-basic_curse!(BasicCurse, "Curse", 0, -1);
+/// [Wiki link](http://wiki.dominionstrategy.com/index.php/Silver)
+#[derive(Clone, Serialize, Deserialize)]
+pub struct Silver;
+
+#[typetag::serde]
+impl Card for Silver {
+    name!("Silver");
+    cost!(3);
+    types!(vec![Treasure]);
+    treasure_value!(2);
+}
+
+/// [Wiki link](http://wiki.dominionstrategy.com/index.php/Gold)
+#[derive(Clone, Serialize, Deserialize)]
+pub struct Gold;
+
+#[typetag::serde]
+impl Card for Gold {
+    name!("Gold");
+    cost!(6);
+    types!(vec![Treasure]);
+    treasure_value!(3);
+}
+
+/// [Wiki link](http://wiki.dominionstrategy.com/index.php/Estate)
+#[derive(Clone, Serialize, Deserialize)]
+pub struct Estate;
+
+#[typetag::serde]
+impl Card for Estate {
+    name!("Estate");
+    cost!(2);
+    types!(vec![Treasure]);
+    victory_points!(1);
+}
+
+/// [Wiki link](http://wiki.dominionstrategy.com/index.php/Duchy)
+#[derive(Clone, Serialize, Deserialize)]
+pub struct Duchy;
+
+#[typetag::serde]
+impl Card for Duchy {
+    name!("Duchy");
+    cost!(5);
+    types!(vec![Treasure]);
+    victory_points!(3);
+}
+
+/// [Wiki link](http://wiki.dominionstrategy.com/index.php/Province)
+#[derive(Clone, Serialize, Deserialize)]
+pub struct Province;
+
+#[typetag::serde]
+impl Card for Province {
+    name!("Province");
+    cost!(8);
+    types!(vec![Treasure]);
+    victory_points!(6);
+}
+
+/// [Wiki link](http://wiki.dominionstrategy.com/index.php/Curse)
+#[derive(Clone, Serialize, Deserialize)]
+pub struct BasicCurse;
+
+#[typetag::serde]
+impl Card for BasicCurse {
+    name!("Curse");
+    cost!(0);
+    types!(vec![Curse]);
+    victory_points!(-1);
+}
