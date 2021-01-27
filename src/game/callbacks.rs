ce06219650ee2a155267a7123765626ffd4cb5c2
Chenkail <40770208+Chenkail@users.noreply.github.com>
2021-01-27 12:35:21 -0800

    Change prompt_card_from_hand callback to usize

--- src/game/callbacks.rs
+++ src/game/callbacks.rs
@@ -3,14 +3,16 @@
 use crate::game::player::Player;
 
 pub type FnToBool = Box<dyn Fn() -> bool>;
-pub type FnPlayerToi32 = Box<dyn Fn(&Player) -> i32>;
+pub type FnToUsize = Box<dyn Fn() -> usize>;
 pub type FnToVecUsize = Box<dyn Fn() -> Vec<usize>>;
+pub type FnPlayerToUsize = Box<dyn Fn(&Player) -> usize>;
+pub type FnPlayerToi32 = Box<dyn Fn(&Player) -> i32>;
 
 pub struct Callbacks {
     /// Is the player done with this phase
     pub prompt_player_done: FnToBool,
     /// Get an index of a card in hand to choose to play
-    pub prompt_card_from_hand: FnPlayerToi32,
+    pub prompt_card_from_hand: FnToUsize,
     /// Get a list of indices of cards from hand
     pub prompt_indices_from_hand: FnToVecUsize,
 }
--- src/sample_client.rs
+++ src/sample_client.rs
@@ -2,7 +2,7 @@
 
 use std::{io, vec};
 
-use crate::game::player::Player;
+// use crate::game::player::Player;
 use crate::game::callbacks::*;
 
 pub fn client() -> Callbacks {
@@ -20,11 +20,11 @@ fn prompt_done() -> bool {
     !input.to_ascii_lowercase().starts_with('y')
 }
 
-fn prompt_card_from_hand(_p: &Player) -> i32 {
+fn prompt_card_from_hand() -> usize {
     let mut input = String::new();
     println!("Enter a card index from your hand:");
     io::stdin().read_line(&mut input).expect("error: unable to read user input");
-    input.parse::<i32>().unwrap()
+    input.parse::<usize>().unwrap()
 }
 
 fn prompt_indices_from_hand() -> Vec<usize> {
