a6b3e2653a7989d4e7d1c9033c4f45c7f77b2eae
Chenkail <40770208+Chenkail@users.noreply.github.com>
2021-02-24 23:28:47 -0800

    Use thiserror shorthand

--- src/error.rs
+++ src/error.rs
@@ -9,9 +9,9 @@ pub type DominionResult = Result<(), DominionError>;
 
 #[derive(Debug, Error)]
 pub enum DominionError {
-    #[error("Card doesn't have expected type! Expected: {:?}", .expected)]
+    #[error("Card doesn't have expected type! Expected: {expected:?}")]
     CardTypeMisMatch { expected: CardType },
-    #[error("Pile is empty: {:?}", .card)]
+    #[error("Pile is empty: {card:?}")]
     EmptyPile { card: Box<dyn Card> },
     #[error("Not enough money to buy that card!")]
     InsufficientFunds,
