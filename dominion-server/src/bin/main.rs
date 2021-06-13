e31485e88bf89590410ae7ee9b864635d1889ae9
circumspect <40770208+circumspect@users.noreply.github.com>
2021-06-13 12:36:57 -0700

    Use everyone() in everyone_but() for server

--- dominion-server/src/bin/main.rs
+++ dominion-server/src/bin/main.rs
@@ -18,7 +18,7 @@ fn single_recipient(player_number: usize) -> Recipients {
 }
 
 fn everyone_but(player_count: usize, player_number: usize) -> Recipients {
-    let mut v = (0..player_count).collect::<Vec<usize>>();
+    let mut v = everyone(player_count);
     v.remove(player_number);
 
     v
