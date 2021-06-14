d3e4de5b79c49a01f0f12746fd0e3e1163aa09d8
circumspect <40770208+circumspect@users.noreply.github.com>
2021-06-14 14:54:18 -0700

    Document PartialGame

--- dominion/src/game/partial_game.rs
+++ dominion/src/game/partial_game.rs
@@ -2,6 +2,11 @@ use crate::game::prelude::*;
 
 use serde::{Serialize, Deserialize};
 
+/// A struct representing the game state that a specific player can see.
+///
+/// These should be generated using `Game::partial_game()` and not constructed
+/// by hand.
+
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct PartialGame {
     pub started: bool,
