5cee2679acb756a6a3667426aafdb101c18bc2b7
circumspect <40770208+circumspect@users.noreply.github.com>
2021-06-16 01:22:13 -0700

    Revert "Derive traits for CSOs"
    This reverts commit 28b8933733ca7bd3ca7a4d5d8ccaba2575354bfc.

--- dominion/src/game/cso.rs
+++ dominion/src/game/cso.rs
@@ -10,31 +10,23 @@ pub trait CSO {
     fn effects(&self, player: Player, card: Box<dyn Card>, supply: Supply);
 }
 
-#[derive(Clone, Serialize, Debug, Deserialize)]
 pub trait Artifact: CSO {}
 
-#[derive(Clone, Serialize, Debug, Deserialize)]
 pub trait Boon: CSO {}
 
-#[derive(Clone, Serialize, Debug, Deserialize)]
 pub trait Event: CSO {
     fn coin_cost(&self) -> usize;
     fn debt_cost(&self) -> usize;
 }
 
-#[derive(Clone, Serialize, Debug, Deserialize)]
 pub trait Hex: CSO {}
 
-#[derive(Clone, Serialize, Debug, Deserialize)]
 pub trait Landmark: CSO {}
 
-#[derive(Clone, Serialize, Debug, Deserialize)]
 pub trait Project: CSO {
     fn coin_cost(&self) -> usize;
 }
 
-#[derive(Clone, Serialize, Debug, Deserialize)]
 pub trait State: CSO {}
 
-#[derive(Clone, Serialize, Debug, Deserialize)]
 pub trait Way: CSO {}
