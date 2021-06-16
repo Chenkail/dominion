4a18c00ade03f76854048f07f224916314615bd4
circumspect <40770208+circumspect@users.noreply.github.com>
2021-06-16 01:26:56 -0700

    Add trait bounds for CSO

--- dominion/src/game/cso.rs
+++ dominion/src/game/cso.rs
@@ -3,30 +3,42 @@
 // Need to allow unused variables for default implementations
 #![allow(unused_variables)]
 
+use dyn_clonable::clonable;
+
 use crate::game::{player::Player, card::Card};
 use crate::types::*;
 
-pub trait CSO {
+#[clonable]
+#[typetag::serde(tag = "cso")]
+pub trait CSO: Clone + Send + Sync {
     fn effects(&self, player: Player, card: Box<dyn Card>, supply: Supply);
 }
 
+#[typetag::serde]
 pub trait Artifact: CSO {}
 
+#[typetag::serde]
 pub trait Boon: CSO {}
 
+#[typetag::serde]
 pub trait Event: CSO {
     fn coin_cost(&self) -> usize;
     fn debt_cost(&self) -> usize;
 }
 
+#[typetag::serde]
 pub trait Hex: CSO {}
 
+#[typetag::serde]
 pub trait Landmark: CSO {}
 
+#[typetag::serde]
 pub trait Project: CSO {
     fn coin_cost(&self) -> usize;
 }
 
+#[typetag::serde]
 pub trait State: CSO {}
 
+#[typetag::serde]
 pub trait Way: CSO {}
